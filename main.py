8b9ee09cd41f0b6eabc40d7833708e64d1372dc3
zkan <zkan.cs@gmail.com>
2020-10-15 11:18:27 +0700

    :sparkles: Try parallelize :bear:

--- main.py
+++ main.py
@@ -8,3 +8,13 @@ print(txt.count())
 
 python_lines = txt.filter(lambda line: 'python' in line.lower())
 print(python_lines.count())
+
+# -----
+
+big_list = range(10000)
+
+# parallelize() turns that iterator into a distributed set of numbers and gives you all the capability of Spark’s infrastructure.
+rdd = sc.parallelize(big_list, 2)
+
+odds = rdd.filter(lambda x: x % 2 != 0)
+print(odds.take(5))
