d4bfbbf070958f3241b5f013789e63287677211d
zkan <zkan.cs@gmail.com>
2020-11-15 14:12:18 +0700

    Encode feature and build pipeline :bear:

--- bank.py
+++ bank.py
@@ -1,3 +1,9 @@
+from pyspark.ml import Pipeline
+from pyspark.ml.feature import (
+    OneHotEncoder,
+    StringIndexer,
+    VectorAssembler
+)
 from pyspark.sql import SparkSession
 
 
@@ -6,4 +12,37 @@ spark = SparkSession.builder \
             .appName('ml-bank') \
             .getOrCreate()
 df = spark.read.csv('bank.csv', header = True, inferSchema = True)
+df.printSchema()
+
+categoricalColumns = [
+    'job',
+    'marital',
+    'education',
+    'default',
+    'housing',
+    'loan',
+    'contact',
+    'poutcome',
+]
+stages = []
+for categoricalCol in categoricalColumns:
+    stringIndexer = StringIndexer(inputCol=categoricalCol, outputCol=categoricalCol + 'Index')
+    encoder = OneHotEncoder(inputCols=[stringIndexer.getOutputCol()],
+                            outputCols=[categoricalCol + 'classVec'])
+    stages += [stringIndexer, encoder]
+
+label_stringIdx = StringIndexer(inputCol='deposit', outputCol='label')
+stages += [label_stringIdx]
+
+numericCols = ['age', 'balance', 'duration', 'campaign', 'pdays', 'previous']
+assemblerInputs = [c + 'classVec' for c in categoricalColumns] + numericCols
+assembler = VectorAssembler(inputCols=assemblerInputs, outputCol='features')
+stages += [assembler]
+print(stages)
+
+pipeline = Pipeline(stages=stages)
+pipelineModel = pipeline.fit(df)
+df = pipelineModel.transform(df)
+selectedCols = ['label', 'features'] + cols
+df = df.select(selectedCols)
 df.printSchema()
